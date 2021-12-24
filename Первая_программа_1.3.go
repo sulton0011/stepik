9cc47a1b7276c074c3f35a2f94837af7ff6bd5de
sulton0011 <azizxonkom@gmail.com>
2021-12-24 11:23:29 +0500

    1.3 I like Go! 0.0

---
+++ Первая_программа_1.3.go
@@ -0,0 +1,11 @@
+// Напишите программу, которая выведет "I like Go!" 3 раза.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+	fmt.Println("I like Go!")
+	fmt.Println("I like Go!")
+}
