6f0bbdb87195f84fc17d5f12823093ff5d1a410a
sulton0011 <azizxonkom@gmail.com>
2021-12-24 12:51:58 +0500

    1.7 Исправьте ошибку в программе ниже: 0.0

---
+++ Константы_1.7.go
@@ -0,0 +1,18 @@
+// Исправьте ошибку в программе ниже:
+
+// Sample Input:
+
+// Sample Output:
+// 18
+
+package main
+
+import "fmt"
+
+func main(){
+    var a int = 8
+    const b int = 10
+    a = a + b
+	
+    fmt.Println(a)
+}
