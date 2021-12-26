84ef6d8978700310854fd2a9799393138dd8e7ba
sulton0011 <azizxonkom@gmail.com>
2021-12-26 17:15:44 +0500

    2.1 Напишите функцию f(), которая будет принимать строку text и выводить ее (печатать на экране).  0.0

---
+++ golang/Функции_2.1_0.0.go
@@ -0,0 +1,24 @@
+// Напишите функцию f(), которая будет принимать строку text и выводить ее (печатать на экране).
+
+// От вас требуется дописать только эту функцию, считайте что функция main() уже объявлена, считывать с
+// консоли ничего не нужно!
+
+// Пакет "fmt" уже импортирован!
+
+// Sample Input: Hello!
+
+// Sample Output: Hello!
+
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	f("Hello!")
+}
+
+func f(text string) {
+	fmt.Println(text)    //print your code here
+}
