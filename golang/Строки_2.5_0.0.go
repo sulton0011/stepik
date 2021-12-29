9daacc95e9269235458d586c52a5fa445833ec92
sulton0011 <azizxonkom@gmail.com>
2021-12-29 20:31:45 +0500

    2.7 Нужно определить, является ли она правильной или нет. 0.0

---
+++ golang/Строки_2.5_0.0.go
@@ -0,0 +1,35 @@
+// На вход подается строка. Нужно определить, является ли она правильной или нет. Правильная строка начинается с
+// заглавной буквы и заканчивается точкой. Если строка правильная - вывести Right иначе - вывести Wrong
+
+// Маленькая подсказка: fmt.Scan() считывает строку до первого пробела, вы можете считать полностью строку за раз с
+// помощью bufio:
+
+// text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+// Sample Input: Быть или не быть.
+
+// Sample Output: Right
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	text = strings.ReplaceAll(text, " ", "")
+
+
+	if unicode.IsUpper(rune(text[0])) && text[utf8.RuneCountInString(text) - 2] == 208{
+		fmt.Println("Right")
+	}else {
+		fmt.Println("Wrong")
+	}
+}
